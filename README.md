99d6809baf9dd232d44135f50e6c5a7dc8eaebe9
Hanne Maes <maes.hanne@gmail.com>
2020-11-04 10:22:37 +0100

    published version

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Smooth Surfing <small style="color:lightgrey">Firefox addon</small>
 Small website injections that make my browsing experience smoother.<br><br>
-<img src="screenshot.png" width="300px" height="300px">
+<img src="screenshot.png" width="441px" height="393px">
 
 # Testing
 1. about:debugging
@@ -14,3 +14,7 @@ Small website injections that make my browsing experience smoother.<br><br>
 - Build: `cd src && web-ext build`
   - Installation: `npm install --global web-ext`
   - Docs: https://extensionworkshop.com/documentation/develop/getting-started-with-web-ext/
+
+# If fullscreen is not working
+1. Go to `about:config` and `set full-screen-api.allow-trusted-requests-only = false`
+
--- src/content_script.js
+++ src/content_script.js
@@ -24,16 +24,50 @@ function messageToBg(message) {
 // todoist: scroll to bottom
 if(windowLocation.includes("todoist")) {
 
-  messageToBg('▼') // messageToBg('↓')
+  messageToBg('▼')
 
   // scroll down when page is loade
   setTimeout(function() {
-    console.log('Smooth browsing: Todoist -> scroll to bottom')
-    window.scrollTo(0, document.body.scrollHeight)
+    scrollToBottom()
+    document.getElementsByTagName('html')[0].mozRequestFullScreen();
   }, 2000);
 } 
+
+//****************************************************************************************************************************************************
+
+// todoist: scroll to bottom
+if(windowLocation.includes("cables.gl/edit")) {
+
+  messageToBg('+')
+
+  // scroll down when page is loade
+  setTimeout(function() {
+    fullscreen()
+    document.getElementsByTagName('html')[0].mozRequestFullScreen();
+  }, 2000);
+} 
+
+//****************************************************************************************************************************************************
 
 // we are not on a site with an action -> set the icon inactive
 else {
   messageToBg('inactive')
 }
+
+//****************************************************************************************************************************************************
+// helper functions
+//****************************************************************************************************************************************************
+
+function scrollToBottom() {
+  console.log('Smooth browsing: Todoist -> scroll to bottom')
+  window.scrollTo(0, document.body.scrollHeight)
+}
+
+//****************************************************************************************************************************************************
+
+function fullscreen() {
+  console.log('Smooth browsing: Todoist -> fullscreen')
+  document.getElementsByTagName('html')[0].mozRequestFullScreen();
+}
+
+//****************************************************************************************************************************************************
